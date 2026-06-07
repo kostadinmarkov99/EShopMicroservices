838207fd220a9c0352a2c98573775a70c001e47c
kostadinmarkov9 <kostadin.markov99@gmail.com>
2026-06-07 22:13:24 +0300

    Untill lection 68

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,21 @@
+using Weasel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+// Add middleware to the HTTP request pipeline.
+app.MapCarter();
+
+app.Run();
